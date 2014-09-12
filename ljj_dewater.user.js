8b912d64fef8bc9f0cf4b99c5553c17ef9d0c8a6
Abby Pan <abbypan@gmail.com>
2014-09-12 11:28:57 +0800

    url

--- ljj_dewater.user.js
+++ ljj_dewater.user.js
@@ -10,10 +10,8 @@
 // @grant         GM_getResourceText
 // @include       http://www.jjwxc.net/onebook.php?novelid=*
 // @exclude       http://www.jjwxc.net/onebook.php?*chapterid=*
-// @resource      bbs_dewater https://raw.github.com/abbypan/bbs_dewater/master/bbs_dewater.js
 // @resource      ljj_dewater https://raw.github.com/abbypan/ljj_dewater/master/ljj_dewater.js
-// @downloadURL   http://userscripts.org/scripts/source/486984.user.js
-// @updateURL     http://userscripts.org/scripts/source/486984.meta.js
+// @resource      bbs_dewater https://github.com/abbypan/bbs_dewater/raw/master/bbs_dewater.js
 // ==/UserScript==
 //
 // --------------------------------------------------------------------
